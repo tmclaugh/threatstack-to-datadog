44e7f464770daf07203f5baa30287f16a1bdc51e
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-02-10 14:49:37 -0500

    Abstract Threat Stack requests.

--- app/models/threatstack.py
+++ app/models/threatstack.py
@@ -34,50 +34,26 @@ class ThreatStackModel:
         Interact with Threat Stack via API.
         '''
 
-    def is_available(self):
+    def _make_threatstack_request(self, api_type, api_type_id=None, api_args=[]):
         '''
-        Check connectivity to Threat Stack.
-
-        Returns a failure if cannot connect to Threat Stack API.  This could be
-        anything from API credential issues to connection failure.
+        Make a request to Threat Stack and get back a response.
         '''
+        url_type = api_type
+        if api_type_id:
+            url_type_id = api_type_id
+        else:
+            url_type_id = ''
 
-        alerts_url = '{}/alerts?count=1'.format(THREATSTACK_BASE_URL)
+        if api_args:
+            url_args = '?{}'.format('&'.join(api_args))
+        else:
+            url_args = ''
 
-        try:
-            resp = requests.get(
-                alerts_url,
-                headers={'Authorization': THREATSTACK_API_KEY}
-            )
-
-        except requests.exceptions.RequestException as e:
-            exc_info = sys.exc_info()
-            if sys.version_info >= (3,0,0):
-                raise ThreatStackRequestError(e).with_traceback(exc_info[2])
-            else:
-                six.reraise(
-                    ThreatStackRequestError,
-                    ThreatStackRequestError(e),
-                    exc_info[2]
-                )
-
-        if not resp.ok:
-            if 'application/json' in resp.headers.get('Content-Type'):
-                raise ThreatStackAPIError(
-                    resp.reason,
-                    resp.status_code,
-                    resp.json()
-                )
-            else:
-                raise ThreatStackRequestError(resp.reason, resp.status_code)
-
-        return True
-
-    def get_alert_by_id(self, alert_id):
-        '''
-        Retrieve an alert from Threat Stack by alert ID.
-        '''
-        alerts_url = '{}/alerts/{}'.format(THREATSTACK_BASE_URL, alert_id)
+        alerts_url = '{}{}'.format(
+            '/'.join([THREATSTACK_BASE_URL, url_type, url_type_id,]),
+            url_args
+        )
+        _logger.info(alerts_url)
 
         try:
             resp = requests.get(
@@ -107,4 +83,25 @@ class ThreatStackModel:
                 raise ThreatStackRequestError(resp.reason, resp.status_code)
 
         return resp.json()
+
+    def is_available(self):
+        '''
+        Check connectivity to Threat Stack.
+
+        Returns a failure if cannot connect to Threat Stack API.  This could be
+        anything from API credential issues to connection failure.
+        '''
+
+        if self._make_threatstack_request('alerts', api_args=['count=1']):
+            available = True
+        else:
+            available = False
+
+        return available
+
+    def get_alert_by_id(self, alert_id):
+        '''
+        Retrieve an alert from Threat Stack by alert ID.
+        '''
+        return self._make_threatstack_request('alerts', alert_id,)
 
