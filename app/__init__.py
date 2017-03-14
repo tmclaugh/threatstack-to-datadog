2341b8fffdde61aa9ccf61f6e08dbbb9091139f4
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-03-14 16:01:04 -0400

    add flask lambda support

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@
 Assemble our service.
 '''
 import config
-from flask import Flask
+from flask_lambda import FlaskLambda
 import logging
 
 _logger = logging.getLogger(__name__)
@@ -26,7 +26,7 @@ def create_app():
     Create an app by initializing components.
     '''
     _logger.info('Initializing application')
-    application = Flask(__name__)
+    application = FlaskLambda(__name__)
 
     _initialize_errorhandlers(application)
     _initialize_blueprints(application)
