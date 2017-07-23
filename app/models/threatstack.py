99e5572649c18f43250afe8a781dfd07a2ab2244
Tom McLaughlin <tom@cloudzero.com>
2017-07-23 16:29:20 -0400

    Switch to theatstack Python module.

--- app/models/threatstack.py
+++ app/models/threatstack.py
@@ -7,6 +7,7 @@ import logging
 import requests
 import six
 import sys
+import threatstack
 
 _logger = logging.getLogger(__name__)
 
@@ -14,11 +15,6 @@ THREATSTACK_API_KEY = config.THREATSTACK_API_KEY
 THREATSTACK_BASE_URL = config.THREATSTACK_BASE_URL
 
 class ThreatStackBaseError(AppBaseError):
-    '''
-    Base Threat Stack error.
-    '''
-
-class ThreatStackRequestError(ThreatStackBaseError):
     '''
     Base Threat Stack error.
     '''
@@ -33,55 +29,7 @@ class ThreatStackModel:
         '''
         Interact with Threat Stack via API.
         '''
-
-    def _make_threatstack_request(self, api_type, api_type_id=None, api_args=[]):
-        '''
-        Make a request to Threat Stack and get back a response.
-        '''
-        url_type = api_type
-        if api_type_id:
-            url_type_id = api_type_id
-        else:
-            url_type_id = ''
-
-        if api_args:
-            url_args = '?{}'.format('&'.join(api_args))
-        else:
-            url_args = ''
-
-        alerts_url = '{}{}'.format(
-            '/'.join([THREATSTACK_BASE_URL, url_type, url_type_id,]),
-            url_args
-        )
-
-        try:
-            resp = requests.get(
-                alerts_url,
-                headers={'Authorization': THREATSTACK_API_KEY}
-            )
-
-        except requests.exceptions.RequestException as e:
-            exc_info = sys.exc_info()
-            if sys.version_info >= (3,0,0):
-                raise ThreatStackRequestError(e).with_traceback(exc_info[2])
-            else:
-                six.reraise(
-                    ThreatStackRequestError,
-                    ThreatStackRequestError(e),
-                    exc_info[2]
-                )
-
-        if not resp.ok:
-            if 'application/json' in resp.headers.get('Content-Type'):
-                raise ThreatStackAPIError(
-                    resp.reason,
-                    resp.status_code,
-                    resp.json()
-                )
-            else:
-                raise ThreatStackRequestError(resp.reason, resp.status_code)
-
-        return resp.json()
+        self._threatstack_client = threatstack.ThreatStack(THREATSTACK_API_KEY)
 
     def is_available(self):
         '''
@@ -91,10 +39,17 @@ class ThreatStackModel:
         anything from API credential issues to connection failure.
         '''
 
-        if self._make_threatstack_request('alerts', api_args=['count=1']):
-            available = True
-        else:
-            available = False
+        available = False
+        try:
+            for org in self._threatstack_client.organizations.list():
+                if org:
+                    available = True
+                    break
+        except threatstack.ThreatStackAPIError as e:
+            raise ThreatStackAPIError(e.message)
+        # Module doesn't handle the None response that occurs on bad API key.
+        except TypeError:
+            pass
 
         return available
 
@@ -102,11 +57,21 @@ class ThreatStackModel:
         '''
         Get agent info for the given ID
         '''
-        return self._make_threatstack_request('agents', agent_id,)
+        try:
+            resp = self._threatstack_client.agents.get(agent_id)
+        except threatstack.ThreatStackAPIError as e:
+            raise ThreatStackAPIError(e)
+
+        return resp
 
     def get_alert_by_id(self, alert_id):
         '''
         Retrieve an alert from Threat Stack by alert ID.
         '''
-        return self._make_threatstack_request('alerts', alert_id,)
+        try:
+            resp = self._threatstack_client.alerts.get(alert_id)
 
+        except threatstack.ThreatStackAPIError as e:
+            raise ThreatStackAPIError(e)
+
+        return resp
